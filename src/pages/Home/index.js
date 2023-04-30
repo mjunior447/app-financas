31a0ee0f29a7146fd4fcb23810d37321d920f797
mjunior447 <mjunior447@gmail.com>
2023-04-30 16:38:24 -0300

    add balance and expenses section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Balance from '../../components/Balance';
 import Header from '../../components/Header';
 
 export default function Home() {
   return (
-    <View>
+    <View style={styles.container}>
         <Header name='Mauricio Junior' />
-      <Text>Sujeitos programadores</Text>
+        <Balance balance='9.250.90' expenses='-527,00' />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FAFAFA'
+    }
+})
