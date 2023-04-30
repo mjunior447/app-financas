d4e92a0e916d23e995e31d83117bfe99336910c3
mjunior447 <mjunior447@gmail.com>
2023-04-30 17:07:08 -0300

    create last movements section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Balance from '../../components/Balance';
 import Header from '../../components/Header';
+import Movements from '../../components/Movements';
+
+const list = [
+    {
+        id: 1,
+        label: 'Boleto conta de luz',
+        value: '300,90',
+        date: '17/09/2022',
+        type: 0 // despesas
+    },
+    {
+        id: 2,
+        label: 'Pix cliente X',
+        value: '2.500,00',
+        date: '21/06/2022',
+        type: 1 // receitas / emtradas
+    },
+    {
+        id: 3,
+        label: 'Salário',
+        value: '7.200,00',
+        date: '03/01/2023',
+        type: 1 // receitas / emtradas
+    }
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
         <Header name='Mauricio Junior' />
         <Balance balance='9.250.90' expenses='-527,00' />
-      <StatusBar style="auto" />
+        <Text style={styles.title}>Últimas movimentações</Text>
+
+        <FlatList
+            style={styles.list}
+            data={list}
+            keyExtractor={(item) => String(item.id)}
+            showsVerticalScrollIndicator={false}
+            renderItem={({item}) => <Movements data={item} />}
+        />
+
+        <StatusBar/>
     </View>
   );
 }
@@ -17,5 +52,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#FAFAFA'
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        margin: 14
+    },
+    list: {
+        marginStart: 14,
+        marginEnd: 14
     }
 })
